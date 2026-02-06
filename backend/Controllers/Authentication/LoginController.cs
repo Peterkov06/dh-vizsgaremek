f4b08eb465636f480188f319d4e598a611bf5770
zsuczko <instabb6464@gmail.com>
2026-02-06 09:50:06 +0100

    backend - creating endpoints: register

---
+++ backend/Controllers/Authentication/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers.Login
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+
+        public LoginController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        public record RegisterDTO(string Email, string Password, string Role, string Full_name, string Address, DateTime Date_of_birth, string? Nickname);
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody]RegisterDTO register) {
+
+            var newUser = new ApplicationUser()
+            {
+                Email = register.Email,
+                FullName = register.Full_name,
+                Address = register.Address,
+                DateOfBirth = register.Date_of_birth
+            };
+
+            if (!string.IsNullOrEmpty(register.Nickname)) {
+                newUser.Nickname = register.Nickname;
+            }
+
+            var result = await _userManager.CreateAsync(newUser, register.Password);
+
+            if (!result.Succeeded) {
+                return BadRequest(result.Errors);
+            }
+
+            if (!string.IsNullOrEmpty(register.Role)) {
+                await _userManager.AddToRoleAsync(newUser, register.Role);
+            }
+
+            return Ok(new { message= "User successfully created!"});
+        }
+    }
+}
--- backend/Models/ApplicationUser.cs
+++ backend/Models/ApplicationUser.cs
@@ -4,6 +4,10 @@ namespace backend.Models
 {
     public class ApplicationUser: IdentityUser
     {
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public string Address { get; set; }
+    
+        public string? Nickname { get; set; }
     }
 }
