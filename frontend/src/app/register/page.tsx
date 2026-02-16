198a96aea194aab4ee872c222cdb816cd6d8c719
Kovacs Peter <peterkov06@gmail.com>
2026-02-16 16:09:56 +0100

    Frontend - small changes before connecting to backend

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -5,7 +5,7 @@ import PersonalDataComponent from "./PersonalDataComponent";
 const page = () => {
   return (
     <div className="min-h-screen w-full bg-registration-bg flex flex-col justify-center items-center px-10">
-      <IntroductionComponent />
+      <MainRegisterComponent />
     </div>
   );
 };
