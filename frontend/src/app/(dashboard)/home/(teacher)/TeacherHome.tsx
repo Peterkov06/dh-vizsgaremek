94162bde9f1568cb2e0be441db712761bb2987e1
zsuczko <instabb6464@gmail.com>
2026-03-06 09:26:53 +0100

    frontend - teacher home pedding students

--- frontend/src/app/(dashboard)/home/(teacher)/TeacherHome.tsx
+++ frontend/src/app/(dashboard)/home/(teacher)/TeacherHome.tsx
@@ -11,12 +11,17 @@ import {
 import { User } from "@/lib/auth";
 import { DashboardModel } from "@/lib/models/homeModel";
 import { TeacherDashboardModel } from "@/lib/models/teacherHome";
-import { Bell } from "lucide-react";
+import { Bell, Check, ChevronRight, X } from "lucide-react";
 import { useEffect, useState } from "react";
 import CourseCard from "./components/CourseCard";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Label } from "@/components/ui/label";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 const TeacherHome = (props: { user: User }) => {
   const [dashboard, setDashboard] = useState<TeacherDashboardModel>();
+  const [selectedTab, setSelectedTab] = useState<string>("s");
+
   async function fetchDashboard() {
     const response = await fetch("mockup/teacherHome.json")
       .then((data) => data.json())
@@ -63,7 +68,7 @@ const TeacherHome = (props: { user: User }) => {
           </div>
         </div>
       </section>
-      <section className="grid grid-cols-4">
+      <section className="grid grid-cols-4 gap-3">
         <section className=" col-span-2">
           <div className="flex justify-between items-center w-fit lg:w-full gap-5">
             <h1 className="text-xl lg:text-2xl font-bold">Kurzusok</h1>
@@ -72,7 +77,6 @@ const TeacherHome = (props: { user: User }) => {
           <Carousel
             opts={{
               align: "start",
-              loop: true,
             }}
             className="relative overflow-visible"
           >
@@ -83,16 +87,86 @@ const TeacherHome = (props: { user: User }) => {
                 </CarouselItem>
               ))}
             </CarouselContent>
-            <CarouselPrevious className="absolute left-[-20] top-1/2 -translate-y-1/2 z-10" />
-            <CarouselNext className="absolute right-[-20] top-1/2 -translate-y-1/2 z-10" />
+            <CarouselPrevious className="absolute left-[-10] top-1/2 -translate-y-1/2 z-10" />
+            <CarouselNext className="absolute right-[-10] top-1/2 -translate-y-1/2 z-10" />
           </Carousel>
         </section>
-        <section className=" col-start-3">
-          <h1>Vami</h1>
+        <section className="col-start-3 border-4 border-[#EBEDEC] p-2 gap-2 rounded-2xl mt-7 grid grid-rows-5">
+          <RadioGroup
+            className="grid grid-cols-2 gap-0"
+            value={selectedTab}
+            onValueChange={setSelectedTab}
+          >
+            <div
+              className={`border-4 border-[#EBEDEC] rounded-l-xl ${selectedTab === "s" ? "bg-background text-primary font-bold" : "bg-[#EBEDEC] text-gray-600"}`}
+            >
+              <RadioGroupItem
+                value="s"
+                className="hidden"
+                id="studs"
+              ></RadioGroupItem>
+              <Label
+                htmlFor="studs"
+                className="h-full w-full flex justify-center items-center"
+              >
+                Tanulók
+              </Label>
+            </div>
+            <div
+              className={`flex justify-center items-center border-4 border-[#EBEDEC] rounded-r-xl ${selectedTab === "m" ? "bg-background text-primary font-bold" : "bg-[#EBEDEC] text-[#898989]"}`}
+            >
+              <RadioGroupItem
+                value="m"
+                className="hidden"
+                id="money"
+              ></RadioGroupItem>
+              <Label
+                htmlFor="money"
+                className="h-full w-full flex justify-center items-center"
+              >
+                Pénzügyek
+              </Label>
+            </div>
+          </RadioGroup>
+          <div className="row-span-3 row-start-2">
+            <h1 className="font-bold text-md">Függőben</h1>
+            <div className="overflow-hidden h-[7em]">
+              <div className="overflow-y-auto h-full flex flex-col gap-2">
+                {dashboard?.pendingEnrollments.map((stud, i) => (
+                  <div className="flex bg-[#EBEDEC] p-2 rounded-xl items-center justify-between">
+                    <div className="flex items-center gap-1">
+                      <Avatar key={i}>
+                        <AvatarImage
+                          src="/defaults/default_avatar.jpg"
+                          alt=""
+                        />
+                      </Avatar>
+                      <div>
+                        <h2 className="text-sm font-bold">{stud.userName}</h2>
+                        <h3 className="text-xs">{stud.courseName}</h3>
+                      </div>
+                    </div>
+                    <div className="flex gap-1">
+                      <Button className="h-8 w-8 bg-linear-to-tl from-primary to-[#7CB08C]">
+                        <Check className="size-6"></Check>
+                      </Button>
+                      <Button className="h-8 w-8 bg-linear-to-tl from-[#B02929] to-[#BD6060]">
+                        <X className="size-6"></X>
+                      </Button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="row-start-5 flex justify-center">
+            <Button className="h-8 w-40 flex gap-1 bg-linear-to-tl from-foreground to-[#868686]">
+              <p>Összes Kérés</p>
+              <ChevronRight className="size-5 m-0"></ChevronRight>
+            </Button>
+          </div>
         </section>
-        <section className="  col-start-4">
-          <h1>Vami2</h1>
-        </section>
+        <section className="col-start-4 border-4 border-[#EBEDEC] rounded-2xl mt-7"></section>
       </section>
     </main>
   );
