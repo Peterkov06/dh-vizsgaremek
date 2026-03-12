d0dcf6d2579b02779aa5329a2f439ed8b831b594
Kovács Péter <peterkov06@gmail.com>
2026-03-12 19:00:48 +0100

    Backend - new migration due to renaming of the dbcontext

---
+++ backend/Migrations/20260312175855_db_context_name_change.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class db_context_name_change : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_CourseReviews_SingleContext",
+                table: "course_feedbacks");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_CourseReviews_SingleContext",
+                table: "course_feedbacks",
+                sql: "((\"WallId\" IS NOT NULL)::int + (\"EnrollmentId\" IS NOT NULL)::int) = 1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_CourseReviews_SingleContext",
+                table: "course_feedbacks");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_CourseReviews_SingleContext",
+                table: "course_feedbacks",
+                sql: "(\"WallId\" IS NOT NULL)::int + (\"EnrollmentId\" IS NOT NULL)::int) = 1");
+        }
+    }
+}
