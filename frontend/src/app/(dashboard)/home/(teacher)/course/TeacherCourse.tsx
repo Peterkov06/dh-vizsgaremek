9b73524f29f787e37499f66b697e20082c1dc1ef
zsuczko <instabb6464@gmail.com>
2026-04-03 15:41:23 +0200

    frontend - latest working frontend

--- frontend/src/app/(dashboard)/home/(teacher)/course/TeacherCourse.tsx
+++ frontend/src/app/(dashboard)/home/(teacher)/course/TeacherCourse.tsx
@@ -1,15 +1,20 @@
 import { Button } from "@/components/ui/button";
 import { User } from "@/lib/auth";
+import { Plus } from "lucide-react";
 import Link from "next/link";
 
 const TeacherCourse = (props: { user: User }) => {
   return (
     <main>
-      <h1 className="text-4xl font-bold text-primary">Kurzusaim</h1>
+      <h1 className="text-5xl font-bold text-primary">Kurzusaim</h1>
 
       <Link href={"course/creator"}>
-        <Button>Kurzus létrehozzása</Button>
+        <Button className="text-2xl h-14">
+          <Plus className="size-8"></Plus>Kurzus létrehozzása
+        </Button>
       </Link>
+
+      <section></section>
     </main>
   );
 };
